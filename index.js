23183b3f539b05f10683c79d21a3719c8abd9023
Diego <diegojairhl@gmail.com>
2022-03-03 20:17:18 -0500

    Modificación de DOM con data interna perfil

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+// Hola, soy Diego Huaman,
+// Puedo crear aplicaciones web fascinantes.
+const documentReady = () => {
+  const heroSubtitle = document.getElementByIdgit a("hero-subtitle");
+  const heroTitle = document.getElementById("hero-title");
+
+  const latestWorkLink = document.getElementById("latest-work-link");
+
+  const profile = ["Hola! Soy Diego Huaman,", "Puedo crear aplicaciones web fascinantes."];
+
+  const workLinks = ["https://darsico.github.io/calculadora-retro/"];
+
+  heroSubtitle.innerHTML = profile[0];
+  heroTitle.innerHTML = profile[1];
+  latestWorkLink.href = workLinks[0];
+};
+
+document.addEventListener("DOMContentLoaded", documentReady);
